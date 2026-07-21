df04e99047d21b2d08d12ac78efb1eae6f809293
Shanmukh095 <shanmukhsatyasaiyerra@gmail.com>
2026-07-21 23:19:19 +0530

    DistilBERT model files have been saved

---
+++ src/models/distilbert/predict.py
@@ -0,0 +1,73 @@
+# src/models/distilbert/predict.py
+
+import pickle
+import torch
+
+from transformers import (
+    DistilBertTokenizer,
+    DistilBertForSequenceClassification
+)
+
+from src.config import LABEL_ENCODER_PATH, MODEL_DIR
+
+
+class ResumePredictor:
+
+    def __init__(self):
+
+        self.tokenizer = (
+            DistilBertTokenizer.from_pretrained(
+                str(MODEL_DIR / "distilbert")
+            )
+        )
+
+        self.model = (
+            DistilBertForSequenceClassification.from_pretrained(
+                str(MODEL_DIR / "distilbert")
+            )
+        )
+
+        with open(
+            LABEL_ENCODER_PATH,
+            "rb"
+        ) as f:
+
+            self.label_encoder = pickle.load(f)
+
+    def predict(self, resume_text):
+
+        inputs = self.tokenizer(
+            resume_text,
+            truncation=True,
+            padding=True,
+            max_length=512,
+            return_tensors="pt"
+        )
+
+        with torch.no_grad():
+
+            outputs = self.model(**inputs)
+
+        probabilities = torch.softmax(
+            outputs.logits,
+            dim=1
+        )
+
+        confidence = torch.max(
+            probabilities
+        ).item()
+
+        predicted_id = torch.argmax(
+            probabilities,
+            dim=1
+        ).item()
+
+        predicted_category = (
+            self.label_encoder
+            .inverse_transform([predicted_id])[0]
+        )
+
+        return {
+            "category": predicted_category,
+            "confidence": round(confidence, 4)
+        }
